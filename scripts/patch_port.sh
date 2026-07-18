bbe8b8de6b295a1527c1fb200708bec6ce536fe6
Qosmos11 <qosmos11@proton.me>
2026-07-18 13:36:17 +0300

    some changes

--- scripts/patch_port.sh
+++ scripts/patch_port.sh
@@ -11,39 +11,24 @@ if [ -d "$DONOR_UNPACK" ]; then
     find "$DONOR_UNPACK" -maxdepth 1 -type f -name "*.img" ! -name "super.img" ! -name "super.img.raw" -exec cp {} "$PORT_DIR/" \; 2>/dev/null
 fi
 
-apply_mod_layer() {
-    local layer_path="$1"
-    local layer_name="$2"
+MODS_LIST=( "${BASE_MODS[@]}" "${TARGET_MODS[@]}"
 
-    if [ -d "$layer_path" ]; then        
-        for mod_dir in "$layer_path"/*; do
-            [ -d "$mod_dir" ] || continue
-            mod_sub_name=$(basename "$mod_dir")
-            for custom_cfg in "$mod_dir"/unified_config-*; do
-                if [ -f "$custom_cfg" ]; then
-                    cfg_target=$(basename "$custom_cfg")          
-                    TMP_MERGE=$(mktemp)
-                    cat "$PORT_META/$cfg_target" "$custom_cfg" 2>/dev/null > "$TMP_MERGE"
-                    tac "$TMP_MERGE" | awk -F'|' '!x[$1]++' | tac > "$PORT_META/$cfg_target"
-                    rm -f "$TMP_MERGE"
-                fi
-            done
-            if [ -f "$mod_dir/patch.sh" ]; then
-                echo "Applying module: $mod_sub_name"
-                STOCK_META_DIR="$STOCK_META"
-                source "$mod_dir/patch.sh"
-            fi
+for mod_path in "${MODS_LIST[@]}"; 
+    mod_dir="./mods/$mod_path"
+    if [ -d "$mod_dir" ]; then
+        for custom_cfg in "$mod_dir"/unified_config-*; do
+             if [ -f "$custom_cfg" ]; then
+                cfg_target=$(basename "$custom_cfg")          
+                TMP_MERGE=$(mktemp)
+                cat "$PORT_META/$cfg_target" "$custom_cfg" 2>/dev/null > "$TMP_MERGE"
+                tac "$TMP_MERGE" | awk -F'|' '!x[$1]++' | tac > "$PORT_META/$cfg_target"
+                rm -f "$TMP_MERGE"
+             fi
         done
-    fi
-}
-
-apply_mod_layer "./source/$BASE/patches" "Source Specific Mods"
-
-apply_mod_layer "./mods" "Global Kitchen Mods"
-
-apply_mod_layer "./target/$TARGET/common" "Device Specific Common Mods"
-
-apply_mod_layer "./target/$TARGET/$BASE" "Hybrid Hybrid-Pair Specific Fixes"
-
+        if [ -f "$mod_dir/patch.sh" ]; then
+            echo "Applying module: $mod_sub_name"
+            STOCK_META_DIR="$STOCK_META"
+            source "$mod_dir/patch.sh"
+        fi
 
 echo "All mods are done!"
